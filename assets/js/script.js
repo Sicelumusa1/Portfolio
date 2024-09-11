c10e3b2ac00636b1a460691da6359136502698fa
Sicelumusa <musaqwabe@gmail.com>
2024-09-11 23:33:52 +0200

    Added typed.js and applied it

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+// Typed.js
+var typed = new Typed('.study-field h2', {
+  strings: ['Software Engineer', 'Web Developer', 'Aspiring Mobile Developer'],
+  loop: true, typeSpeed: 70, backSpeed: 10,
+});
+
